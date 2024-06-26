0d9ecad61edfba0989085248c2371ba681b64cdb
Zimny1 <matmateo2008@gmail.com>
2024-06-26 19:06:20 +0200

    added better style

--- src/dzien2_frontend/src/Components/Blog.vue
+++ src/dzien2_frontend/src/Components/Blog.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
         <h2 class="text-blue-600">Wpisy na bloga</h2>
-        <button @click="pobierzWpisy">refresh</button>
-        <div v-for="wpis in wpisy">
-            <p>{{ wpis }}</p>
+        
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="float-right bg-blue-600 rounded-md text-white p-4">refresh</button>
         </div>
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="wpis in wpisy" 
+            class="drop-shadow-xl bg-stone-300">
+                <p>{{ wpis }}</p>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4">
+            <button @click="dodajWpisy" class="float-right bg-blue-600 rounded-md text-white p-4">dodaj</button>
+        </div>
     </div>
 </template>
 
